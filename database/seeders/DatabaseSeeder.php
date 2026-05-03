8146682f5edfc578c57aca2869c5cc7c3b25ee37
NameeNomen <fat.lp3i33@gmail.com>
2026-05-03 19:36:23 +0700

    test10

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ProdukSeeder::class,
             HardwareSoftwareProdukSeeder::class,
-            UserSeeder::class
+            // UserSeeder::class
         ]);
     }
 }
